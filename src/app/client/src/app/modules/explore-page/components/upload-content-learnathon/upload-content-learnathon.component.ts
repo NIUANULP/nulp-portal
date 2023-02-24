ac0683955a12faf5248127e60a2e115e62ebc322
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-02-24 13:45:54 +0530

    chore : added learnathon upload content component

---
+++ src/app/client/src/app/modules/explore-page/components/upload-content-learnathon/upload-content-learnathon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-content-learnathon',
+  templateUrl: './upload-content-learnathon.component.html',
+  styleUrls: ['./upload-content-learnathon.component.scss']
+})
+export class UploadContentLearnathonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
