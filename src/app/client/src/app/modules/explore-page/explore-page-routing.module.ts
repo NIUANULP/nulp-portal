93926c5f0cba70b8f3f7a1eb241c6ca65b12cc4c
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-02-27 15:24:50 +0530

    chore : added files erased previously

--- src/app/client/src/app/modules/explore-page/explore-page-routing.module.ts
+++ src/app/client/src/app/modules/explore-page/explore-page-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ExplorePageComponent } from './components';
+import { UploadContentLearnathonComponent } from './components/upload-content-learnathon/upload-content-learnathon.component';
 const routes: Routes = [
   {
     path: '', component: ExplorePageComponent, data: {
+      telemetry: {
+        env: 'explore', pageid: 'explore', type: 'view', subtype: 'paginate'
+      },
+      softConstraints: { badgeAssertions: 98, board: 99, channel: 100 }
+    }
+  },
+  {
+    path: 'learnathon', component: UploadContentLearnathonComponent, data: {
       telemetry: {
         env: 'explore', pageid: 'explore', type: 'view', subtype: 'paginate'
       },
--- src/app/client/src/app/modules/explore-page/explore-page.module.ts
+++ src/app/client/src/app/modules/explore-page/explore-page.module.ts
@@ -17,9 +17,10 @@ import { SlickModule } from 'ngx-slick';
 import { ExplorePageComponent } from './components';
 import { ContentSectionModule } from 'content-section';
 import {ObservationModule} from '../observation/observation.module';
- 
+import { UploadContentLearnathonComponent } from './components/upload-content-learnathon/upload-content-learnathon.component';
+import { CommonFormElementsModule } from 'common-form-elements-web-v9'; 
 @NgModule({
-  declarations: [ExplorePageComponent],
+  declarations: [ExplorePageComponent, UploadContentLearnathonComponent],
   imports: [
     ExplorePageRoutingModule,
     CommonModule,
@@ -30,7 +31,7 @@ import {ObservationModule} from '../observation/observation.module';
     SharedFeatureModule,
     SuiSelectModule, SuiModalModule, SuiAccordionModule, SuiPopupModule, SuiDropdownModule, SuiProgressModule,
     SuiRatingModule, SuiCollapseModule, SuiDimmerModule, WebExtensionModule,
-    CommonConsumptionModule, ContentSearchModule, SlickModule, ContentSectionModule,ObservationModule
+    CommonConsumptionModule, ContentSearchModule, SlickModule, ContentSectionModule,ObservationModule,CommonFormElementsModule
   ]
 })
 export class ExplorePageModule { }
