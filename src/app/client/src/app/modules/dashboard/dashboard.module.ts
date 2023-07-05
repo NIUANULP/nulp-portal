fd62b8a1dc8311aa7a53b2a3afbcf29eb9c560f0
Shivani Parit <58217306+paritshivani@users.noreply.github.com>
2023-07-05 12:38:51 +0530

    Revert "Upgrade customisation"

--- src/app/client/src/app/modules/dashboard/dashboard.module.ts
+++ src/app/client/src/app/modules/dashboard/dashboard.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonConsumptionModule } from '@project-sunbird/common-consumption-v9';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown/angular2-multiselect-dropdown';
 // Modules
 import { ChartsModule } from 'ng2-charts';
 import { SuiModule } from 'ng2-semantic-ui-v9';
@@ -18,12 +17,10 @@ import {
 import {
   OrganisationComponent, CourseConsumptionComponent, CourseProgressComponent, UsageReportsComponent,
   DataTableComponent, DataChartComponent, ReportComponent, ReportSummaryComponent, ListAllReportsComponent,
-  AddSummaryModalComponent, CourseDashboardComponent, ReIssueCertificateComponent, DashboardSidebarComponent, DatasetComponent, MapComponent, FilterComponent,AllReportsComponent, ReportsComponent,OrganizationReportComponent, UserReportComponent,ContentReportComponent, ContentLeaderboardComponent,ContentCategoryWiseComponent, CourseReportComponent,CourseCategoryWiseComponent,
-  ContentCreationStaticsComponent,CityWiseReportComponent, DeptCityWiseReportComponent, ContentDeptWiseReportComponent
+  AddSummaryModalComponent, CourseDashboardComponent, ReIssueCertificateComponent, DashboardSidebarComponent,
+  DatasetComponent, MapComponent, FilterComponent
 } from './components';
 import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
-// import { ChartsModule } from 'ng2-charts';
-import { ChartModule } from 'primeng/chart';
 // SB core and shared services
 import { SearchService } from '@sunbird/core';
 import { SharedModule } from '@sunbird/shared';
@@ -37,29 +34,9 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { SharedFeatureModule } from '@sunbird/shared-feature';
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import { AddusersComponent } from './components/addusers/addusers.component';
-import { TableModule } from 'primeng/table';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { DropdownModule } from 'primeng/dropdown';
-import { UserSearchService } from '../search/services';
-import { UserUploadComponent } from './components/user-upload/user-upload.component';
-import { CalendarModule } from 'primeng/calendar';
-import { CoreModule } from '../core';
-import { CourseContentLeaderboardComponent } from './components/course-content-leaderboard/course-content-leaderboard.component';
-import { CertificateTemplateUploadComponent } from './components/certificate-template-upload/certificate-template-upload.component';
-import { FileuploadComponent } from './components/fileupload/fileupload.component';
-import { OrgManagementModule } from '@sunbird/org-management';
-import { CertificateDirectivesModule } from 'sb-svg2pdf';
-import { CsModule } from '@project-sunbird/client-services';
-import { CsLibInitializerService } from 'CsLibInitializer';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
-export const csCourseServiceFactory = (csLibInitializerService: CsLibInitializerService) => {
-  if (!CsModule.instance.isInitialised) {
-    csLibInitializerService.initializeCs();
-  }
-  return CsModule.instance.courseService;
-};
+
 @NgModule({
   imports: [
     CommonModule,
@@ -81,34 +58,18 @@ export const csCourseServiceFactory = (csLibInitializerService: CsLibInitializer
     MatSlideToggleModule,
     MatExpansionModule,
     SharedFeatureModule,
-    MatAutocompleteModule,
-    TableModule,
-    DropdownModule,
-    MultiSelectModule,
-    AngularMultiSelectModule,
-    CalendarModule,
-    CoreModule,
-    ChartModule,
-    CertificateDirectivesModule,
-    OrgManagementModule
+    MatAutocompleteModule
   ],
   declarations: [CourseConsumptionComponent, OrganisationComponent, CourseProgressComponent, UsageReportsComponent,
     DataTableComponent, DataChartComponent, ListAllReportsComponent, ReportSummaryComponent, ReportComponent, AddSummaryModalComponent,
-    CourseDashboardComponent, ReIssueCertificateComponent, DashboardSidebarComponent, DatasetComponent, MapComponent, FilterComponent,
-    SbTableComponent, AddusersComponent, AllReportsComponent, UserReportComponent, ContentLeaderboardComponent,UserUploadComponent,
-    CourseContentLeaderboardComponent, FileuploadComponent, CertificateTemplateUploadComponent, ReportsComponent,OrganizationReportComponent,ContentReportComponent, ContentCategoryWiseComponent,
-    CourseReportComponent,CourseCategoryWiseComponent, ContentCreationStaticsComponent,CityWiseReportComponent, DeptCityWiseReportComponent, ContentDeptWiseReportComponent],
+    CourseDashboardComponent, ReIssueCertificateComponent, DashboardSidebarComponent, DatasetComponent, MapComponent, FilterComponent, SbTableComponent],
   exports: [CourseProgressComponent, DataTableComponent],
   providers: [
     RendererService,
-    UserSearchService,
     DashboardUtilsService,
     SearchService,
     LineChartService,
     CourseConsumptionService,
-    OrganisationService, DownloadService, CourseProgressService, UsageService, ReportService,
-    [{provide: 'CS_COURSE_SERVICE', useFactory: csCourseServiceFactory, deps: [CsLibInitializerService]}]
-  ],
-  
+    OrganisationService, DownloadService, CourseProgressService, UsageService, ReportService]
 })
 export class DashboardModule { }
