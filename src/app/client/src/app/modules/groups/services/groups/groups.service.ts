be475aeb0bcd1a7a6b313e39b3f43a0caeea2bc3
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-26 21:37:15 +0530

    chore : added search functionality for discussion forum

--- src/app/client/src/app/modules/groups/services/groups/groups.service.ts
+++ src/app/client/src/app/modules/groups/services/groups/groups.service.ts
@@ -391,6 +391,7 @@ getActivity(groupId, activity, mergeGroup, leafNodesCount?) {
     };
 
     option.data.request.filters['rootOrgId'] = this.userService.rootOrgId;
+    option.data.request['limit'] = 500;
 
     return this.learnerService.post(option).pipe(map((data) => {
       return data;
