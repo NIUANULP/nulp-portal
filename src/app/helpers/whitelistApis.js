3cdcb11c30b79e2d8e7d464d68924c12c5013468
snehalsabade <snehal.sabade@tekditechnologies.com>
2024-01-22 17:08:46 +0530

    unable to edit organisation - whitelist forbidden error

--- src/app/helpers/whitelistApis.js
+++ src/app/helpers/whitelistApis.js
@@ -624,6 +624,14 @@ const API_LIST = {
     },
 
     // ORD related APIs
+    'learner/org/v1/read': {
+      checksNeeded: ['ROLE_CHECK'],
+      ROLE_CHECK: [ROLE.ADMIN]
+    },
+    'learner/org/v1/update': {
+      checksNeeded: ['ROLE_CHECK'],
+      ROLE_CHECK: [ROLE.ADMIN]
+    },  
     '/learner/org/v2/search': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.PUBLIC]
@@ -2067,7 +2075,9 @@ const API_LIST = {
     '/learner/user/v1/upload',
     '/learner/user/v1/role/assign',
     '/learner/notification/v1/feed/read/:uid',
-    '/learner/course/v1/batch/participants/list'
+    '/learner/course/v1/batch/participants/list',
+    '/learner/org/v1/read',
+    'learner/org/v1/update'
   ]
 };
 module.exports = API_LIST;
