71227630c06ba5b7ca7a85545fc93eeeba77379b
vky25 <93203895+vky25@users.noreply.github.com>
2024-06-06 16:19:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -79,8 +79,8 @@ build_server(){
     cp -R libs helpers proxy resourcebundles package.json framework.config.js sunbird-plugins routes constants controllers server.js ./../../Dockerfile app_dist
     
     # Copy additional files to dist folder
-    echo "copying additional files to dist"
-    cp -r /var/lib/jenkins/elite-ui/nulp-elite-ui/prod-build/webapp /var/lib/jenkins/workspace/Build/Core/Player/src/app/app_dist/dist/webapp
+    #echo "copying additional files to dist"
+    #cp -r /var/lib/jenkins/elite-ui/nulp-elite-ui/prod-build/webapp /var/lib/jenkins/workspace/Build/Core/Player/src/app/app_dist/dist/webapp
     
     
     cd app_dist
