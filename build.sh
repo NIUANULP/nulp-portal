166e172295d5458a96e95f309dc930960ae1a5b1
Manoj Nath <66711855+ManojNathIC@users.noreply.github.com>
2024-08-13 12:42:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 STARTTIME=$(date +%s)
 CLIENT_NODE_VERSION=14.20.0
-SERVER_NODE_VERSION=18.0.0
+SERVER_NODE_VERSION=16.19.0
 echo "Starting portal build from build.sh"
 set -euo pipefail	
 export NVM_DIR="$HOME/.nvm"
