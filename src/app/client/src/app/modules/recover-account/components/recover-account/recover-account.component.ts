e4984a040c66eabadd2a398af7bf75be44ccfd63
sonaligarud <sonali.garud@tekditechnologies.com>
2024-08-22 14:35:27 +0530

    Task #225451 - [FE] - UI Revamp for Recover account - Forgot Password

--- src/app/client/src/app/modules/recover-account/components/recover-account/recover-account.component.ts
+++ src/app/client/src/app/modules/recover-account/components/recover-account/recover-account.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RecoverAccountService } from './../../services';
 import { ResourceService } from '@sunbird/shared';
 
+
 @Component({
   templateUrl: './recover-account.component.html',
   styleUrls: ['./recover-account.component.scss']
 })
 export class RecoverAccountComponent {
-
+ nulp_logo= "../../../../../assets/images/nulp_logo.png";
+  
   constructor(public recoverAccountService: RecoverAccountService, public resourceService: ResourceService) { }
 
 }
