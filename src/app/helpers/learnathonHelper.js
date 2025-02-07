ac4d6ad5b225c2d62b0309c32bb0754f7af6718d
Manee8943 <maneeshakrishnan@tarento.com>
2025-02-07 12:27:17 +0530

    decrypt code added

--- src/app/helpers/learnathonHelper.js
+++ src/app/helpers/learnathonHelper.js
@@ -35,6 +35,18 @@ const encrypt = (text) => {
   return encryptedData;
 };
 
+
+const decrypt = (encryptedData) => {
+  const textParts = encryptedData?.split(":");
+  const iv = Buffer?.from(textParts?.shift(), "hex");
+  const encryptedText = Buffer.from(textParts?.join(":"), "hex");
+  let decipher = crypto?.createDecipheriv("aes-256-cbc", key, iv);
+  let decrypted = decipher?.update(encryptedText);
+  decrypted = Buffer?.concat([decrypted, decipher?.final()]);
+  return decrypted?.toString();
+};
+ 
+
 const createLearnathonContent = async (req, res) => {
   try {
     const url = `${envHelper.api_base_url}/learner/user/v5/read/${req?.session?.userId}?fields=organisations,roles,locations,declarations,externalIds`;
@@ -303,6 +315,14 @@ const listLearnathonContents = async (req, res) => {
     values.push(parseInt(limit), parseInt(offset));
 
     const result = await getRecords(query, values);
+    result?.rows?.forEach((row) => {
+      if (row.email) {
+        row.email = decrypt(row.email);
+      }
+      if (row.mobile_number) {
+        row.mobile_number = decrypt(row.mobile_number);
+      }
+    })
 
     let countQuery = `
       SELECT COUNT(*) 
