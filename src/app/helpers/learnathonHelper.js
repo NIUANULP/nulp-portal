3269f13db2407b369b9a0b11244f16a8b540c13e
Manee8943 <maneeshakrishnan@tarento.com>
2025-02-28 11:42:30 +0530

    date for create form and update form

--- src/app/helpers/learnathonHelper.js
+++ src/app/helpers/learnathonHelper.js
@@ -76,7 +76,7 @@ const createLearnathonContent = async (req, res) => {
       });
     }
     const today = dayjs();
-    if (today.isAfter("2025-02-28 23:59:59")) {
+    if (today.isAfter("2025-02-28 12:30:00")) {
       return res.status(403).send({
         ts: new Date().toISOString(),
         params: {
@@ -444,7 +444,7 @@ const updateLearnathonContent = async (req, res) => {
     }
 
     const today = dayjs();
-    if (today.isAfter("2025-02-28 23:59:59")) {
+    if (today.isAfter("2025-02-28 12:30:00")) {
       return res.status(403).send({
         ts: new Date().toISOString(),
         params: {
