effa44c984efe2040be12b5a702efe8be3fa8627
Suraj Kumar <103109628+suraj-tekdi@users.noreply.github.com>
2025-05-16 12:54:02 +0530

    Customer Request #240251 Fix: Country label not displaying issue fix (#692)
    * Customer Request #240251 Fix: country label not displsying
    
    * Customer Request #239785 Fix: country label issue fix

--- src/app/client/src/app/modules/public/module/signup/components/signup-basic-info/signup-basic-info.component.ts
+++ src/app/client/src/app/modules/public/module/signup/components/signup-basic-info/signup-basic-info.component.ts
@@ -51,10 +51,6 @@ export class SignupBasicInfoComponent implements OnInit {
     public utilService: UtilService, public configService: ConfigService, private _fb: FormBuilder, private signupService: SignupService) { }
 
   ngOnInit(): void {
-    console.log('ngOnInit called');
-    console.log("Country", this.resourceService.frmelmnts.lbl.profile.Country)
-    console.log("selectCountry", this.resourceService.frmelmnts.lbl.profile.selectCountry)
-    console.log("enterCountryName", this.resourceService.frmelmnts.lbl.profile.enterCountryName)
     this.instance = _.upperCase(this.resourceService.instance || 'NULP');
     this.loadStates();  // Fetch state list from API
     this.personalInfoForm = this._fb.group({
