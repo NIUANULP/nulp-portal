bf7be4ee3c725972fbfbb32f2e493c5a16383fa2
Komal Patil <komal_m@tekditechnologies.com>
2025-06-18 17:01:47 +0530

    Jun 18 Prod Deployment (#708)
    * TASK #00000 :chnage event library version to to change event cenrtificate template (#688)
    
    * Customer Request #240251: Feat: Country selects box needs to be added on registration page (#690)
    
    * Task #240186: Email send functionalities using sunbird for nodebb
    
    * Task #240186: Email send functionalities using sunbird for nodebb
    
    * country field added
    
    ---------
    
    Co-authored-by: ManojNathIC <manoj_n@tekditechnologies.com>
    Co-authored-by: Manoj Nath <66711855+ManojNathIC@users.noreply.github.com>
    
    * Customer Request #240251 Fix: country label not displsying (#691)
    
    * Customer Request #240251 Fix: Country label not displaying issue fix (#692)
    
    * Customer Request #240251 Fix: country label not displsying
    
    * Customer Request #239785 Fix: country label issue fix
    
    * Release 4.10.0 rc12 elite prod dev (#697)
    
    * Customer Request #240251 Fix: country label not displsying
    
    * Customer Request #239785 Fix: country label issue fix
    
    * Customer Request #24025 Feat: update country userInfo
    
    * Customer Request - Bug #242175 fix: workspace is accessible to learnathon creator (#707)
    
    * Customer Request #240251 Fix: country label not displsying
    
    * Customer Request #239785 Fix: country label issue fix
    
    * Customer Request #24025 Feat: update country userInfo
    
    * Customer Request - Bug #242175 fix: workspace is accessible to learnathon creator
    
    * TASK #00000 : Cahnaged Event Library version for evetn certificate changes (#699)
    
    ---------
    
    Co-authored-by: Mahesh Mahajan <134256523+mahajanmahesh935@users.noreply.github.com>
    Co-authored-by: Suraj Kumar <103109628+suraj-tekdi@users.noreply.github.com>
    Co-authored-by: ManojNathIC <manoj_n@tekditechnologies.com>
    Co-authored-by: Manoj Nath <66711855+ManojNathIC@users.noreply.github.com>

--- src/app/helpers/customHelper.js
+++ src/app/helpers/customHelper.js
@@ -124,6 +124,7 @@ async function updateUserInfo(req, res) {
       updated_by,
       user_type,
       organisation,
+      country,
       state,
       district,
       state_id,
@@ -145,12 +146,13 @@ async function updateUserInfo(req, res) {
           user_type = COALESCE($3, user_type), 
           organisation = COALESCE($4, organisation), 
           updated_by = COALESCE($5, updated_by),
-          state = COALESCE($6, state),
-          district = COALESCE($7, district),
-          state_id = COALESCE($8, state_id),
-          district_id = COALESCE($9, district_id),
+          country = COALESCE($6, country),
+          state = COALESCE($7, state),
+          district = COALESCE($8, district),
+          state_id = COALESCE($9, state_id),
+          district_id = COALESCE($10, district_id),
           updated_at = NOW() 
-        WHERE user_id = $10
+        WHERE user_id = $11
         RETURNING *`;
 
       const values = [
@@ -159,6 +161,7 @@ async function updateUserInfo(req, res) {
         user_type || null,
         organisation || null,
         updated_by || null,
+        country || null,
         state || null,
         district || null,
         state_id || null,
@@ -183,8 +186,8 @@ async function updateUserInfo(req, res) {
     } else {
       // If user does not exist, perform an insert
       const query = `
-        INSERT INTO users (user_id, designation, bio, created_by, user_type, organisation, state, district, state_id, district_id) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) 
+        INSERT INTO users (user_id, designation, bio, created_by, user_type, organisation, country, state, district, state_id, district_id) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) 
         RETURNING *`;
 
       const values = [
@@ -194,6 +197,7 @@ async function updateUserInfo(req, res) {
         updated_by,
         user_type || null,
         organisation || null,
+        country || null,
         state || null,
         district || null,
         state_id || null,
